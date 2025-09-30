641ff85523f4298d0cd8755657f11433cc7b9301
a00575206-droid <a00575206@tec.mx>
2025-09-29 21:07:25 -0600

    Update win_inorganica_Pedro.py

--- docs/win_inorganica_Pedro.py
+++ docs/win_inorganica_Pedro.py
@@ -1,4 +1,1 @@
-Pedro Hernández Avalos - A00575206
--Ventana: Basura Inorgánica
 
--Añadí una ventana asignada a la basura inorgánica. Esta se actualizará para que se abra esta ventana después del escaneo de la imagen, relacionándose con la ventana que trabajo Elian. Como añadido puede abrir otra ventana que muestra un poco más de información. 
